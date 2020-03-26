f134d6f27fb1f696bd521628463bb4705fe0e00b
Curzey <carstena1994@gmail.com>
2020-03-26 16:32:09 +0100

    Added CTAs to /home. fixes #7

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,11 +3,39 @@
     <div>
       <h1 class="display-3"><span class="fat">Master Refridgiator</span> at your service!</h1>
       <p class="lead d-flex align-items-center justify-content-center">
-        See more at <a class="d-flex align-items-center" href="https://github.com/Curzey/Fridge-Stock"><i class="gg-git-fork"></i> Github</a>
+        See more at <a class="d-flex align-items-center" target="_blank" href="https://github.com/Curzey/Fridge-Stock"><i class="gg-git-fork"></i> Github</a>
       </p>
+      <section class="cta">
+        <b-button :variant="isNewUser ? 'link' : 'primary'" to="/fridge-stock">Tilføj madvarer</b-button>
+        <b-button :variant="!isNewUser ? 'link' : 'primary'" to="/user">Opret dit første table</b-button>
+      </section>
     </div>
   </div>
 </template>
+
+<script>
+import api from '@/api'
+
+export default {
+  data () {
+    return {
+      activeUser: null,
+      isNewUser: true
+    }
+  },
+  async created () {
+    this.activeUser = await this.$auth.getUser()
+    this.isNewUser = await this.updateIsNewUser()
+  },
+  methods: {
+    async updateIsNewUser () {
+      const tables = await api.getElements('tables')
+      const userTables = tables.filter(item => item.user === this.activeUser.sub)
+      return userTables.length <= 0
+    }
+  }
+}
+</script>
 
 <style lang="postcss">
   .hero {
@@ -17,15 +45,28 @@
     justify-content: center;
     text-align: center;
   }
+
   .hero .lead {
     font-weight: 200;
     font-size: 1.5rem;
   }
+
   .gg-git-fork {
     margin-left: 8px;
     margin-right: 10px;
   }
+
   .fat {
     font-weight: 600;
+  }
+
+  .cta {
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+  }
+
+  .cta .btn-link {
+    order: 2;
   }
 </style>
