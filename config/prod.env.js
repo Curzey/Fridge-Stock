06049e4eb71b2e5a0ead229fa31888ea3033ff59
Curzey <carstena1994@gmail.com>
2021-05-11 18:45:19 +0200

    Update prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_OKTA_REDIRECT_URI: '"http://app.curzey.dk/implicit/callback"',
-  VUE_APP_API_BASEURL: '"http://api.curzey.dk/"'
+  VUE_APP_OKTA_REDIRECT_URI: '"http://159.89.104.237/implicit/callback"',
+  VUE_APP_API_BASEURL: '"http://159.89.104.237/"'
 }
