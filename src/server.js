dfe220b59e8a8618b671b09be3c2980f1c6ec851
curzey <carstena1994@gmail.com>
2022-04-25 15:13:54 +0200

    Added dev storage path
    Should probably be env vars

--- src/server.js
+++ src/server.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
 
 let database = new Sequelize({
   dialect: 'sqlite',
+  // storage: './fridge-db.sqlite',
   storage: '/var/www/html/app/Fridge-Stock/fridge-db.sqlite'
 })
 
